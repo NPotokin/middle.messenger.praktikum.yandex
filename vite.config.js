6077cdc84e706092dd1e411dba490a3faf947af2
NPotokin <npotokin@gmail.com>
2024-03-28 14:38:38 +0300

    Обновить vite.config, создать базовую структуру проекта, начать наботать с компонентом кнопки

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,18 @@
 import {defineConfig} from 'vite';
 import handlebars from 'vite-plugin-handlebars'
+import {resolve} from 'path'
 
-export default defineConfig();
+
+
+export default defineConfig({
+    root: resolve(__dirname, 'src'),
+    build: {
+        outDir: resolve(__dirname, 'dist')
+    },
+    plugins: [
+        handlebars(),
+    ],
+    css: {
+        postcss,
+    } 
+});
