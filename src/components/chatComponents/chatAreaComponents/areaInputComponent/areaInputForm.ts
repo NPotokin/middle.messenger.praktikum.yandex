02424a234e676bf43480836021ea21999e63d2e7
NPotokin <npotokin@gmail.com>
2024-04-27 14:44:37 +0300

    Did from and validatation for message input

--- src/components/chatComponents/chatAreaComponents/areaInputComponent/areaInputForm.ts
+++ src/components/chatComponents/chatAreaComponents/areaInputComponent/areaInputForm.ts
@@ -1,10 +1,12 @@
 import Block from "../../../../core/Block.ts";
 import ChatInput from './chatInput.ts';
-import { ArrowButton} from '../../../../ui/index.ts';
+import { ArrowButton, ErrorLine} from '../../../../ui/index.ts';
 import chatInputValidator from "../../../../utils/inputValidators/chatInputValidator.ts";
 
 interface AreaInputFormInteface{
     message?: string,
+    ErrorText?: string,
+    onBlur?: ()=>void,
 }
 
 export default class AreaInputForm extends Block{
@@ -19,6 +21,10 @@ export default class AreaInputForm extends Block{
                 src: '/icons/arrow-right.svg',
                 buttonType: 'submit',
             }),
+            ErrorLine: new ErrorLine({
+                ...props,
+                error: props.ErrorText,
+            })
         })
     }
 
@@ -26,7 +32,7 @@ export default class AreaInputForm extends Block{
         if(oldProps === newProps){
           return false;
         }
-        this.children.ChatInputField.setProps(newProps);
+        this.children.ErrorLine.setProps(newProps);
         return true;
       }
 
@@ -46,19 +52,32 @@ export default class AreaInputForm extends Block{
 
   
 
-    //submit function
-    //to-do -> console.log message
+    
+    //to-do -> fix blur event with fixing types
     onMessage(e:Event){
         e.preventDefault()
-        console.log('messege sent')
-    }
+        this.children.ChatInputField.props.onBlur
+
+
+        const message = this.props.message
+        console.log(message)
+            
+        }
+        
+      
+        
+        
+    
 
 
 
     render(): string {
         return(`
         <form class="chatAreaInput__input">
-            {{{ChatInputField}}}
+            <div class="chatAreaInput__inputWithError">
+                {{{ChatInputField}}}
+                {{{ErrorLine}}}
+            </div>
             <div class="chatAreaInput__send">
                 {{{ArrowButton}}}
             </div>
--- src/utils/inputValidators/chatInputValidator.ts
+++ src/utils/inputValidators/chatInputValidator.ts
@@ -1,12 +1,13 @@
 export default function chatInputValidator(e: { target: { value: string; }; }) {
-  const inputValue = e.target.value
-  const isValid = inputValue.length !== 0;
+  const inputValue = e.target.value.trim(); // Remove leading and trailing whitespaces
+  const isValid = /.+/.test(inputValue); // Regular expression for validation
+
 
 
   if (isValid) {
-    this.children.ChatInputField.setProps({ error: false, placeHolder: 'Сообщение' });
+    this.children.ErrorLine.setProps({ error: false, ErrorText: null });
   } else {
-    this.children.ChatInputField.setProps({ error: true, placeHolder: 'Пусто!' });
+    this.children.ErrorLine.setProps({ error: true, ErrorText: 'Сообщение не может быть пустым' });
   }
 
   this.setProps({message: inputValue});
